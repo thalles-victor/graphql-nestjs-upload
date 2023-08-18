406fdec76125d0e4aef8658632c44a8e2c048066
thalles-victor <thalles.victor2012@gmail.com>
2023-08-18 18:23:35 -0300

    :fire: remove: remove front end

--- frontend/apollo.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  client: {
-    service: {
-      name: "graphql",
-      url: "http://localhost:3000/graphql",
-    },
-  },
-};
--- frontend/src/lib/apollo.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { createUploadLink } from "apollo-upload-client";
-
-export const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: createUploadLink({ uri: "http://localhost:3000/graphql" }),
-});
